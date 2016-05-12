ddb0daf6d9442be01d59f5c9b20edbba73300282
Milan Damen <github@darvit.nl>
2016-05-12 10:04:21 +0200

    Added models

---
+++ src/model/AND.java
@@ -0,0 +1,15 @@
+package model;
+
+public class AND extends Node {
+
+	@Override
+	public boolean getState() {
+		for (Node node : inputNodes) {
+			if (!node.getState()) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+}
---
+++ src/model/NOT.java
@@ -0,0 +1,5 @@
+package model;
+
+public class NOT extends Node {
+
+}
