3a270f19a429f9f79ae2c78701edc196efe3ebf2
Milan Damen <github@darvit.nl>
2016-06-02 13:40:35 +0200

    Correct panel sizes

--- src/view/MainFrame.java
+++ src/view/MainFrame.java
@@ -1,6 +1,7 @@
 package view;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridLayout;
 
@@ -17,28 +18,32 @@ public class MainFrame extends JFrame {
     
     public MainFrame() {
         this.setTitle("DP1 Simulator");
-      
-        ioPanel = new IOPanel();    
-        ioPanel.setBounds(0, 400, 300, 300);
-
-        circuitPanel = new CircuitPanel();    
-        circuitPanel.setBounds(200, 0, 500, 400);
-        
-        logPanel = new LogPanel();    
-        logPanel.setBounds(400, 300, 400, 300);
+        this.setLayout (null);
+        this.setMinimumSize(new Dimension(1000, 700));
+        this.setResizable(false);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
         controlPanel = new ControlPanel();    
-        controlPanel.setBounds(0, 0, 400, 200);
+        controlPanel.setBounds(0, 0, 200, 400);
+        controlPanel.setBackground(Color.MAGENTA);
+        
+        circuitPanel = new CircuitPanel();    
+        circuitPanel.setBounds(200, 0, 800, 400);
+        circuitPanel.setBackground(Color.BLUE);
+      
+        ioPanel = new IOPanel();
+        ioPanel.setBounds(0, 400, 300, 300);
+        ioPanel.setBackground(Color.RED);
+        
+        logPanel = new LogPanel();    
+        logPanel.setBounds(300, 400, 700, 300);
+        logPanel.setBackground(Color.GREEN);
         
         this.add(ioPanel);
         this.add(circuitPanel);
         this.add(logPanel);
         this.add(controlPanel);
         
-        this.setLayout (null);
-        this.setMinimumSize(new Dimension(1000, 700));
-        this.setResizable(false);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setVisible(true);
     }
 }
